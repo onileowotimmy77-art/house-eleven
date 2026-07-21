0ceb9e26a5c5ccacd66c80b69164bd3df5398405
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-21 19:32:23 +0100

    Auto-commit: src/features/journal/article/JournalArticle.tsx at 7/21/2026, 7:32:20 PM

--- src/features/journal/article/JournalArticle.tsx
+++ src/features/journal/article/JournalArticle.tsx
@@ -52,7 +52,7 @@ export default function JournalArticle({
       </EditorialSection>
 
       {/* First Image */}
-      <section className="px-8">
+      <Section customPadding="py-45">
         <div className="relative mx-auto aspect-[16/9] max-w-7xl overflow-hidden">
           <Image
             src={article.gallery[0].image}
