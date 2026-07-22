97678fb708fd4cc7972d6c8bac41287b5701aeec
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-22 17:08:02 +0100

    Add CheckoutSection component with basic structure and layout

---
+++ components/checkout/CheckoutSection.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Section from "@/components/layout/Section";
+import Container from "@/components/layout/Container";
+
+export default function CheckoutSection() {
+  return (
+    <Section customPadding="py-40">
+
+      <Container>
+
+        <div className="max-w-5xl">
+
+          {/* Identity */}
+
+          {/* Delivery */}
+
+          {/* Payment */}
+
+          {/* Review */}
+
+        </div>
+
+      </Container>
+
+    </Section>
+  );
+}
