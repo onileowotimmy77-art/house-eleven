32b39e45bb535700566df70d27e085e22a0a1ccc
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-22 12:10:52 +0100

    Add BagItems component to display items in the shopping bag with empty state handling

---
+++ components/bag/BagItems.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import BagItem from "./BagItem";
+import CommerceEmpty from "@/components/commerce/CommerceEmpty";
+
+const bagItems = [
+  {
+    id: "residence-polo-black",
+
+    image: "/products/residence-polo/01.jpg",
+
+    name: "Residence Polo",
+
+    collection: "Residence",
+
+    colour: "Black",
+
+    size: "M",
+
+    quantity: 1,
+
+    price: 145000,
+  },
+
+  {
+    id: "residence-polo-cream",
+
+    image: "/products/residence-polo/02.jpg",
+
+    name: "Residence Polo",
+
+    collection: "Residence",
+
+    colour: "Cream",
+
+    size: "L",
+
+    quantity: 1,
+
+    price: 145000,
+  },
+];
+
+export default function BagItems() {
+  if (bagItems.length === 0) {
+    return (
+      <CommerceEmpty
+        eyebrow="Selection"
+        title="Nothing Has Been Selected."
+        description="The House is waiting. Explore the collection and discover the pieces that belong with you."
+        cta="Explore Collections"
+        href="/collections"
+      />
+    );
+  }
+
+  return (
+    <div>
+      {bagItems.map((item) => (
+        <BagItem
+          key={item.id}
+          image={item.image}
+          name={item.name}
+          collection={item.collection}
+          colour={item.colour}
+          size={item.size}
+          quantity={item.quantity}
+          price={item.price}
+          onIncrease={() => {}}
+          onDecrease={() => {}}
+          onRemove={() => {}}
+        />
+      ))}
+    </div>
+  );
+}
