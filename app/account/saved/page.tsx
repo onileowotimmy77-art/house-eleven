1effb48212fc670ca0d0cc8d580746b4b894dc12
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-23 10:31:29 +0100

    Add SavedPiecesPage component with AccountLayout and initial structure

---
+++ app/account/saved/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import AccountLayout from "@/components/account/AccountLayout";
+
+export default function SavedPiecesPage() {
+  return (
+    <AccountLayout
+      title="Saved Pieces"
+      description="Your personal archive of House Eleven pieces."
+    >
+      <div className="grid gap-12 md:grid-cols-2 xl:grid-cols-3">
+
+        {/* SavedPieceCard components */}
+
+      </div>
+    </AccountLayout>
+  );
+}
