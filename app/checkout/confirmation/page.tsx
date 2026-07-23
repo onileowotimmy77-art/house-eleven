03da42ba7662e709ad24297a577e9afe2e8c3c99
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-23 09:23:28 +0100

    Auto-commit: app/checkout/confirmation/page.tsx at 7/23/2026, 9:23:27 AM

--- app/checkout/confirmation/page.tsx
+++ app/checkout/confirmation/page.tsx
@@ -13,7 +13,7 @@ import ConfirmationActions from "@/components/confirmation/ConfirmationActions";
 export default function ConfirmationPage() {
   return (
     <>
-      <CommerceHeader
+      <CommerceHeader className="pb-28"
         eyebrow=""
         description=""
         title="Confirmation"
