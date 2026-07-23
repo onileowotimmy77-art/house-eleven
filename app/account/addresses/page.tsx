9cab424906bd01862fd301a8493162ea48bde56d
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-23 10:41:25 +0100

    Add AddressesPage component to manage user addresses in AccountLayout

---
+++ app/account/addresses/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import AccountLayout from "@/components/account/AccountLayout";
+
+export default function AddressesPage() {
+  return (
+    <AccountLayout
+      title="Addresses"
+      description="Manage the destinations where your Residence will arrive."
+    >
+      <div className="space-y-8">
+
+        {/* AddressCard components */}
+
+      </div>
+    </AccountLayout>
+  );
+}
