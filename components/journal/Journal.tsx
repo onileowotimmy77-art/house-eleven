52bba981c2b31bfc7a6768efc1223c2406f40eeb
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-24 05:10:52 +0100

    Refactor Journal component to use JournalFeature and remove JournalCard instances

--- components/journal/Journal.tsx
+++ components/journal/Journal.tsx
@@ -1,41 +1,15 @@
 "use client";
 
-
-import JournalCard from "./JournalCard";
 import Container from "@/components/layout/Container";
-import Section from "../layout/Section";
-import Stagger from "../motion/Stagger";
+import Section from "@/components/layout/Section";
 
+import JournalFeature from "./JournalFeature";
 
 export default function Journal() {
   return (
-    <Section
-      customPadding="py-44"
-    >
+    <Section customPadding="py-44">
       <Container>
-
-      
-        <Stagger className="space-y-40 mt-10">
-
-          <JournalCard
-            image="/journal1.jpg"
-            category="Essay"
-            title="Why We Built House Eleven"
-          />
-
-          <JournalCard
-            image="/journal2.jpg"
-            category="Campaign"
-            title="The Future of Quiet Luxury"
-          />
-
-          <JournalCard
-            image="/journal3.jpg"
-            category="Architecture"
-            title="Architecture Becomes Fashion"
-          />
-        </Stagger>
-
+        <JournalFeature />
       </Container>
     </Section>
   );
