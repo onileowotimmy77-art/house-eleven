b7f59cf1e55ca8cc416cc84051651c8c778aed72
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-25 08:16:20 +0100

    Auto-commit: src/homepage/hero/HeroMedia.tsx at 7/25/2026, 8:16:20 AM

--- src/homepage/hero/HeroMedia.tsx
+++ src/homepage/hero/HeroMedia.tsx
@@ -31,7 +31,7 @@ export default function HeroMedia() {
       }}
     >
       <Image
-        src="/hero.jpg"
+        src="/her"
         alt="House Eleven Chapter 01 Campaign"
         fill
         priority
