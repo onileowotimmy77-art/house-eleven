cdef86ff52a50a0051ac21d522e4decbb36f4093
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-25 07:19:33 +0100

    Auto-commit: components/entrance/EntranceProvider.tsx at 7/25/2026, 7:19:33 AM

--- components/entrance/EntranceProvider.tsx
+++ components/entrance/EntranceProvider.tsx
@@ -58,7 +58,7 @@ export function EntranceProvider({
     if (!lenis) return;
 
     lenis.scrollTo("#manifesto-header", {
-      offset: -120,
+      offset: -80,
       duration: 2.8,
       easing: (t) => 1 - Math.pow(1 - t, 5),
     });
