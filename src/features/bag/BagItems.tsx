9346c6f4110b72e730be2663e3a15f84938922fa
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-26 20:03:54 +0100

    Auto-commit: src/features/bag/BagItems.tsx at 7/26/2026, 8:03:53 PM

--- src/features/bag/BagItems.tsx
+++ src/features/bag/BagItems.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useMemo } from "react";
-
 import BagItem from "./BagItem";
 
-import CommerceEmpty from "../commerce/CommerceEmpty";
+import CommerceEmpty from "@/src/features/commerce/CommerceEmpty";
 
 import { useBag } from "@/src/lib/hooks/useBag";
 
@@ -17,44 +15,7 @@ export default function BagItems() {
     updateBagQuantity,
   } = useBag();
 
-  const bagItems = useMemo(() => {
-    return items
-      .map((item) => {
-        const product = getProduct(item.productSlug);
-
-        if (!product) {
-          return null;
-        }
-
-        return {
-          id: ${item.productSlug}-${item.size},
-
-          image: product.bagImage,
-
-          name: product.name,
-
-          collection: product.collection,
-
-          colour: product.color,
-
-          size: item.size,
-
-          quantity: item.quantity,
-
-          price: product.priceValue,
-
-          productSlug: item.productSlug,
-        };
-      })
-      .filter(
-        (
-          item
-        ): item is NonNullable<typeof item> =>
-          item !== null
-      );
-  }, [items]);
-
-  if (bagItems.length === 0) {
+  if (items.length === 0) {
     return (
       <CommerceEmpty
         eyebrow="Selection"
@@ -68,38 +29,46 @@ export default function BagItems() {
 
   return (
     <div>
-      {bagItems.map((item) => (
-        <BagItem
-          key={item.id}
-          image={item.image}
-          name={item.name}
-          collection={item.collection}
-          colour={item.colour}
-          size={item.size}
-          quantity={item.quantity}
-          price={item.price}
-          onIncrease={() =>
-            updateBagQuantity(
-              item.productSlug,
-              item.size,
-              item.quantity + 1
-            )
-          }
-          onDecrease={() =>
-            updateBagQuantity(
-              item.productSlug,
-              item.size,
-              item.quantity - 1
-            )
-          }
-          onRemove={() =>
-            removeFromBag(
-              item.productSlug,
-              item.size
-            )
-          }
-        />
-      ))}
+      {items.map((item) => {
+        const product = getProduct(item.productSlug);
+
+        if (!product) {
+          return null;
+        }
+
+        return (
+          <BagItem
+            key={`${item.productSlug}-${item.size}`}
+            image={product.bagImage}
+            name={product.name}
+            collection={product.collection}
+            colour={product.color}
+            size={item.size}
+            quantity={item.quantity}
+            price={product.priceValue}
+            onIncrease={() =>
+              updateBagQuantity(
+                item.productSlug,
+                item.size,
+                item.quantity + 1
+              )
+            }
+            onDecrease={() =>
+              updateBagQuantity(
+                item.productSlug,
+                item.size,
+                item.quantity - 1
+              )
+            }
+            onRemove={() =>
+              removeFromBag(
+                item.productSlug,
+                item.size
+              )
+            }
+          />
+        );
+      })}
     </div>
   );
 }
