a794beb111de4807ff78227a1d7d359e97f97f81
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-27 15:23:06 +0100

    Remove BagProvider from RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
                         <Concierge />
                         {children}
                       </LenisProvider>
-                    </BagProvider>
+                    
                   </EntranceProvider>
               
                 </AnimationProvider>
