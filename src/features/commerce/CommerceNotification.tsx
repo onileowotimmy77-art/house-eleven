ee7cd5284857d476474069ad88021e51627895c5
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-07-31 15:09:06 +0100

    Fix formatting issue in CommerceNotification component

--- src/features/commerce/CommerceNotification.tsx
+++ src/features/commerce/CommerceNotification.tsx
@@ -78,7 +78,7 @@ export default function CommerceNotification({
             aria-label="Dismiss notification"
             onClick={() =>
               onDismiss?.();
-            }}
+            }
             initial={{
               opacity: 0,
             }}
