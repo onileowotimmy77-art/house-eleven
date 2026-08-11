7bb0deab716ec58278c1198ecbb0a6d81c865be5
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-08-11 10:18:47 +0100

    Auto-commit: src/lib/stores/useEarlyAccessStore.ts, src/lib/stores/useInventoryStore.ts at 8/11/2026, 10:18:47 AM

--- src/lib/stores/useEarlyAccessStore.ts
+++ src/lib/stores/useEarlyAccessStore.ts
@@ -83,6 +83,7 @@ export const useEarlyAccessStore =
       {
         name:
           "house-eleven-early-access",
+        skipHydration
       }
     )
   );
--- src/lib/stores/useInventoryStore.ts
+++ src/lib/stores/useInventoryStore.ts
@@ -149,7 +149,7 @@ export const useInventoryStore =
       {
         name:
           "house-eleven-inventory",
-        skipHydrat
+        skipHydration: true,
       }
     )
   );
