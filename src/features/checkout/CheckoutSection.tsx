7db84d3a9a9b0e7364727ee3508dfe9711e871fd
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-08-15 21:03:09 +0100

    Auto-commit: src/features/checkout/CheckoutSection.tsx at 8/15/2026, 9:03:08 PM

--- src/features/checkout/CheckoutSection.tsx
+++ src/features/checkout/CheckoutSection.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 import Section from "@/components/layout/Section";
 import Container from "@/components/layout/Container";
 import CheckoutIdentity from "./CheckoutIdentity";
@@ -7,26 +9,34 @@ import CheckoutDelivery from "./CheckoutDelivery";
 import CheckoutPayment from "./CheckoutPayment";
 import CheckoutReview from "./CheckoutReview";
 
+export type CheckoutPaymentMethod =
+  | "card"
+  | "bank";
+
 export default function CheckoutSection() {
+  const [paymentMethod, setPaymentMethod] =
+    useState<CheckoutPaymentMethod>("card");
+
   return (
     <Section customPadding="py-40">
-
       <Container>
-
         <div className="max-w-5xl">
-
           <CheckoutIdentity />
 
           <CheckoutDelivery />
 
-          <CheckoutPayment />
+          <CheckoutPayment
+            paymentMethod={paymentMethod}
+            onPaymentMethodChange={
+              setPaymentMethod
+            }
+          />
 
-          <CheckoutReview />
-
+          <CheckoutReview
+            paymentMethod={paymentMethod}
+          />
         </div>
-
       </Container>
-
     </Section>
   );
 }
