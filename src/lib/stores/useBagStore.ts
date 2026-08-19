832d8d97899288dd6d4f1e9b97f734eb9e452ad6
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-08-19 18:03:43 +0100

    Auto-commit: src/lib/stores/useBagStore.ts at 8/19/2026, 6:03:43 PM

--- src/lib/stores/useBagStore.ts
+++ src/lib/stores/useBagStore.ts
@@ -2,6 +2,7 @@
 
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { useInventoryStore } from "./useInventoryStore";
 
 import type { ProductInventory } from "@/src/data/inventory";
 
