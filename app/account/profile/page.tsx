ae655927a1884fbf47b9ab951044a4d4499df82a
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-09-01 12:04:08 +0100

    Add AccountAccess component to ProfilePage for enhanced account management

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -2,6 +2,7 @@
 
 import AccountLayout from "@/src/features/account/AccountLayout";
 import ProfileInformation from "@/src/features/account/ProfileInformation";
+import AccountAccess from "@/src/features/account/AccountAccess";
 
 export default function ProfilePage() {
   return (
