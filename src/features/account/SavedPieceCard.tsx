4d3a0beb04df1c2a215664b029eb777c5ff209a9
onileowotimmy77-art <onileowotimmy77@gmail.com>
2026-09-06 12:05:14 +0100

    Add inventory store hook to SavedPieceCard component

--- src/features/account/SavedPieceCard.tsx
+++ src/features/account/SavedPieceCard.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
+import { useInventoryStore } from "@/src/lib/stores/useInventoryStore";
 
 interface SavedPieceCardProps {
   image: string;
